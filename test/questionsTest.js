4363684d9cbdbd4323cfac655e5389b207aa286c
Johnpaul <bohrplus@gmail.com>
2018-08-17 22:56:45 +0100

    Test GET/questions/:questionId [#159861896]
    - Add coverage with coverall
    - Add nyc instanbul record
    - Add README Badge for coveralls

--- test/questionsTest.js
+++ test/questionsTest.js
@@ -16,7 +16,34 @@ describe('Questions API integration testing', () => {
         .get('/api/questions')
         .end((err, res) => {
           res.should.have.status(200);
+          res.body.should.be.a('array');
+        });
+
+      done();
+    });
+  });
+
+  describe('#GET: /api/questions/:questionId', () => {
+    it('should get a single question', (done) => {
+      chai.request(router)
+        .get('/api/questions/3')
+        .end((err, res) => {
+          res.should.have.status(200);
           res.body.should.be.a('object');
+          res.body.should.have.property('questionBy');
+          res.body.should.have.property('AddedOn');
+          res.body.should.have.property('answers');
+          res.body.should.have.property('Topics');
+          res.body.should.have.property('answersGiven');
+        });
+
+      done();
+    });
+    it('should return "There is no question with the given id"', (done) => {
+      chai.request(router)
+        .get('/api/questions/20')
+        .end((err, res) => {
+          res.should.have.status(404);
         });
 
       done();
