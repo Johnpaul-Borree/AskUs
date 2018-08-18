a1badb30fc2f3f9b4c37d0e5581538f16bae48aa
Johnpaul <bohrplus@gmail.com>
2018-08-18 19:08:51 +0100

    POST /questions/<quesionId>/answers[#159870000]
    Create route to answer a question by selecting question by
    questionId, then post answer to questions

--- api/routes/questions.js
+++ api/routes/questions.js
@@ -1,6 +1,11 @@
 import express from 'express';
 
-import { getAllQuestions, getQuestionById, createQuestion } from '../controllers/questionsControllers';
+import {
+  getAllQuestions,
+  getQuestionById,
+  createQuestion,
+  answerQuestion,
+} from '../controllers/questionsControllers';
 
 
 const router = express.Router();
@@ -8,9 +13,14 @@ const router = express.Router();
 // GET /questions
 router.get('/', getAllQuestions);
 
+// GET /questions/<questionId>
 router.get('/:questionId', getQuestionById);
 
+// POST /questions
 router.post('/', createQuestion);
+
+// POST /questions/<questionId>/answers
+router.post('/:questionId/answers', answerQuestion);
 
 
 export default router;
