4dd4b2c99e982fcb6d3a180f9d5b84c7ec146f12
Johnpaul <bohrplus@gmail.com>
2018-08-19 18:52:44 +0100

    Add build folder to hold es5 transpiled from babel

---
+++ build/api/models/questionsModels.js
@@ -0,0 +1,49 @@
+'use strict';
+
+exports.questions = [{
+  id: 1,
+  questionBy: 'Mikel Newmann',
+  AddedOn: new Date('December 17, 2016 03:24:00'),
+  question: 'How do I add External Javascript to html',
+  answers: '1820 Answers',
+  Topics: ['html', 'javascript'],
+  answersGiven: [{
+    id: 1,
+    AnsweredBy: 'John Lens',
+    AddedOn: new Date('January 11, 2018 03:24:00'),
+    Answer: 'add a link tag to the head or body, head is prefered. e.g <link href="style.css(/css/dir)" rel="stylesheet"'
+  }]
+}, {
+  id: 2,
+  questionBy: 'John Cena',
+  AddedOn: new Date('January 17, 2018 03:24:00'),
+  question: 'How to use Modal in javascript',
+  answers: '120 Answers',
+  Topics: ['html', 'javascript', 'css3'],
+  answersGiven: []
+}, {
+  id: 3,
+  questionBy: 'Linda Ifechinyere',
+  AddedOn: new Date('March 07, 2018 03:24:00'),
+  question: 'How can I reset password on XAMPP',
+  answers: '90 Answers',
+  Topics: ['PHP', 'MySql', 'XAMPP'],
+  answersGiven: []
+}, {
+  id: 4,
+  questionBy: 'Mr. Jude',
+  AddedOn: new Date('December 19, 2017 03:24:00'),
+  question: 'How to create pointer border with css3',
+  answers: '182 Answers',
+  Topics: ['html5', 'css3'],
+  answersGiven: []
+}, {
+  id: 5,
+  questionBy: 'Vivian Agbo',
+  AddedOn: new Date('December 21, 2016 03:24:00'),
+  question: 'How do I use stored procedures in code first ASP.Net',
+  answers: '420 Answers',
+  Topics: ['ASP.Net MVC5', 'Entity Framework', 'Code First'],
+  answersGiven: []
+}];
+//# sourceMappingURL=questionsModels.js.map
