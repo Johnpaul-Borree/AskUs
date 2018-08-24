33c9320d15fd1b363fb21e83284a5a306777beed
Johnpaul <bohrplus@gmail.com>
2018-08-24 02:38:51 +0100

    create model for user table entities[#159983133]
    - Create a class to hold all user fields
    - Create signup method to enable user signup
    - Create Login method to enable user Login
    - Create a method to check for user email already existing
      before signup
    - Create a method to get a user
    - export to usercontrollers

--- src/server.js
+++ src/server.js
@@ -1,27 +1,14 @@
 import dotenv from 'dotenv';
 import express from 'express';
-// import questions from './api/routes/questions';
-import client from './api/models/dbConnect';
+import routes from './api/routes/routes';
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use('/api/v1/', questions);
+app.use('/api/v1/', routes);
 app.set('json spaces', 40);
-
-
-const text = 'INSERT INTO ask_user( username, first_name, last_name, email, hashed_password) VALUES($1, $2, $3, $4, $5) RETURNING *';
-const values = ['brianc', 'bernard', 'Luisa', 'brian.m.carlson@gmail.com', '1233345'];
-
-client.query(text, values)
-  .then((result) => {
-    console.log(result.rows[0]);
-    // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
-  })
-  .catch(e => console.error(e.stack));
-
 
 const port = process.env.PORT || 3000;
 
