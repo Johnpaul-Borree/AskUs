a5ee45ea553862fa05d6aca5f8bc4ddd8045425c
Johnpaul <bohrplus@gmail.com>
2018-08-27 00:45:45 +0100

    Add a route to get a question [#159983393]
    - Create controller action for this feature
    - Get a question from the database using id

--- src/api/controllers/ask_questions_controller.js
+++ src/api/controllers/ask_questions_controller.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import verifyToken from '../../middleware/authorize';
 import Question from '../models/ask_questions';
-// import User from '../models/ask_user';
 
 const router = express.Router();
 
@@ -21,6 +20,21 @@ router.get('/questions', (req, res) => {
       res.status(500).json({ status: 'failed', message: 'Problem getting questions' });
     });
 });
+// GET A QUESTION
+router.get('/questions/:questionId', (req, res) => {
+  question.getAnyQuestion(req.params.questionId)
+    .then((singleQuestion) => {
+      if (singleQuestion) {
+        res.status(200).json({ singleQuestion, status: 'success', message: 'returned one question' });
+        return;
+      }
+      res.status(404).json({ status: 'success', message: 'no result found' });
+    })
+    .catch(() => {
+      res.status(500).json({ status: 'failed', message: 'cannot get question' });
+    });
+});
+
 
 
 export default router;
