842da4f289c97375ee2b8a6544cb644decea8f6b
Johnpaul <bohrplus@gmail.com>
2018-08-29 17:34:48 +0100

    modified package.json

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import routeMiddleware from './api/routes/routes';
-
+import dbconnect from './dbScema/dbConnect';
 dotenv.config();
 const app = express();
 
@@ -13,6 +13,8 @@ app.set('json spaces', 40);
 routeMiddleware(app);
 
 const port = process.env.PORT || 3000;
+
+console.log(dbconnect);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
