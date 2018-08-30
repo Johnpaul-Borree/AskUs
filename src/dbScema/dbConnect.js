ff7e4e19cbf1f3e4ab852428325e2e5d741d3fdc
Johnpaul <bohrplus@gmail.com>
2018-08-30 10:15:48 +0100

    modified dbconnection

--- src/dbScema/dbConnect.js
+++ src/dbScema/dbConnect.js
@@ -1,6 +1,14 @@
 import { Pool } from 'pg';
 
-const dbConnection = process.env.DB_CONNECTION_URL || 'localhost://postgres:2geda4my99@localhost:5432/askUs';
+const dbConnection = process.env.DATABASE_URL || 'localhost://postgres:2geda4my99@localhost:5432/askUs';
+
+// const dbConnection = {
+//   Host: 'ec2-23-21-236-249.compute-1.amazonaws.com',
+//   User: 'ehxudtpcxeogvl',
+//   SSL: true,
+//   Database: 'd3s5uivsbggcf5',
+//   Password: '3861600c6074f952ce4de7e01b603c22de734ccebd6754703bd67c854fda86f0',
+// };
 
 const pool = new Pool({
   connectionString: dbConnection,
