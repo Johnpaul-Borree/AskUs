6e69501e8ea33e0238b5846b8530201be869ff89
Johnpaul <bohrplus@gmail.com>
2018-08-30 09:32:54 +0100

    connecting to heroku

--- src/dbScema/dbConnect.js
+++ src/dbScema/dbConnect.js
@@ -1,8 +1,6 @@
 import { Pool } from 'pg';
 
 const dbConnection = process.env.DB_CONNECTION_URL || 'localhost://postgres:2geda4my99@localhost:5432/askUs';
-// const dbConnection = 'postgres://ehxudtpcxeogvl:3861600c6074f952ce4de7e01b603c22de734ccebd6754703bd67c854fda86f0@ec2-23-21-236-249.compute-1.amazonaws.com:5432/d3s5uivsbggcf5';
-
 
 const pool = new Pool({
   connectionString: dbConnection,
